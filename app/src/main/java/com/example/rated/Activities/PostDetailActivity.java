13fcb31a966957b929b5290e45ec0e7cb4cf34c5
yoi <amirulhakim47@gmail.com>
2019-03-31 16:29:32 +0800

    display comments on RV update v 0.1

--- app/src/main/java/com/example/rated/Activities/PostDetailActivity.java
+++ app/src/main/java/com/example/rated/Activities/PostDetailActivity.java
@@ -47,7 +47,7 @@ public class PostDetailActivity extends AppCompatActivity{
       FirebaseAuth firebaseAuth;
       FirebaseUser firebaseUser;
       RecyclerView detailRecyclerView;
-      PostDetailAdapter postDetail;
+      PostDetailAdapter postDetailAdapter;
       FirebaseDatabase firebaseDatabase;
       DatabaseReference firebaseReference;
       List<Comment> commentList;
@@ -58,7 +58,7 @@ public class PostDetailActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
-
+        
         // set the status bar to transparent
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
@@ -66,8 +66,8 @@ public class PostDetailActivity extends AppCompatActivity{
 
         // ini views
         imgPost = findViewById(R.id.post_detail_img);
-        imgUserPost = findViewById(R.id.post_detail_user_img);
-        imgCurrentUser = findViewById(R.id.post_view_poster_img);
+        imgCurrentUser = findViewById(R.id.post_detail_user_img);
+        imgUserPost = findViewById(R.id.post_view_poster_img);
 
         txtPostTitle = findViewById(R.id.post_detail_title);
         txtPostDesc = findViewById(R.id.post_detail_desc);
@@ -76,7 +76,7 @@ public class PostDetailActivity extends AppCompatActivity{
         editTextComment = findViewById(R.id.post_detail_comment);
         btnAddComment = findViewById(R.id.post_detail_add_comment);
 
-
+        commentList = new ArrayList<>();
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
@@ -85,13 +85,15 @@ public class PostDetailActivity extends AppCompatActivity{
         detailRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         detailRecyclerView.setHasFixedSize(true);
 
+        postDetailAdapter = new PostDetailAdapter(PostDetailActivity.this,commentList);
+        detailRecyclerView.setAdapter(postDetailAdapter);
+
         firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseReference = firebaseDatabase.getReference("Comment");
+        /*firebaseReference = firebaseDatabase.getReference("Comment");
         firebaseReference.keepSynced(true);
 
-        commentList = new ArrayList<>();
-        postDetail = new PostDetailAdapter(PostDetailActivity.this,commentList);
-        detailRecyclerView.setAdapter(postDetail);
+
+
         //.child(firebaseUser.getUid())
 
         firebaseReference.addValueEventListener(new ValueEventListener() {
@@ -100,14 +102,16 @@ public class PostDetailActivity extends AppCompatActivity{
 
                 for (DataSnapshot data1 : dataSnapshot.getChildren()) {
 
-                    Comment comment = data1.getValue(Comment.class);
-                    /*Comment cmt = new Comment();
+                    Comment comment1 = data1.getValue(Comment.class);
+                    *//*Comment cmt = new Comment();
                     cmt.setUid(data1.getValue(Comment.class).getUid());
                     cmt.setContent(data1.getValue(Comment.class).getContent());
-                    cmt.setUimg(data1.getValue(Comment.class).getUimg());*/
-                    commentList.add(comment);
-                    postDetail.notifyDataSetChanged();
+                    cmt.setUimg(data1.getValue(Comment.class).getUimg());*//*
+                    commentList.add(comment1);
+
                 }
+
+                postDetailAdapter.notifyDataSetChanged();
 
             }
 
@@ -115,7 +119,7 @@ public class PostDetailActivity extends AppCompatActivity{
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Toast.makeText(PostDetailActivity.this, "Opsss.... Something is wrong", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
         //add comment button listener
 
@@ -164,8 +168,8 @@ public class PostDetailActivity extends AppCompatActivity{
         String postDescription = getIntent().getExtras().getString("description");
         txtPostDesc.setText(postDescription);
 
-        String postRating = getIntent().getExtras().getString("rating");
-        txtPostDateName.setText(postRating);
+        String posterName = getIntent().getExtras().getString("userName");
+        txtPostDateName.setText(posterName);
 
         /*String postContent = getIntent().getExtras().getString("content");
         contentDetail.setText(postContent);*/
@@ -182,38 +186,29 @@ public class PostDetailActivity extends AppCompatActivity{
 
         /*String date = timestampToString(getIntent().getExtras().getLong("postDate"));
         txtPostDateName.setText(date);*/
-
+        readComments();
 
     }
 
+    private void showMessage(String message) {
 
-   /* private void iniViews() {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+    }
 
-        *//*firebaseDatabase = FirebaseDatabase.getInstance();
-        firebaseDetail = firebaseDatabase.getReference("Comment");
 
-        detailRecyclerView = findViewById(R.id.post_detail_rv);
-        detailRecyclerView.setHasFixedSize(true);
-        detailRecyclerView.setLayoutManager(new LinearLayoutManager(this));*//*
+    private void readComments() {
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("Comment").child(PostKey);
 
-       // firebaseDetail = FirebaseDatabase.getInstance().getReference("Comment").child(PostKey);
-        String uid = firebaseUser.getUid();
-
-        DatabaseReference commentRef = firebaseDatabase.getReference("Comment");
-        commentRef.addValueEventListener(new ValueEventListener() {
+        mRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                CommentList = new ArrayList<>();
-                  for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
-
-                    Comment comment = dataSnapshot1.getValue(Comment.class);
-                    CommentList.add(comment);
+                commentList.clear();
+                for ( DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Comment comment = snapshot.getValue(Comment.class);
+                    commentList.add(comment);
                 }
+                postDetailAdapter.notifyDataSetChanged();
 
-                postDetail = new PostDetailAdapter(PostDetailActivity.this,CommentList);
-                detailRecyclerView.setAdapter(postDetail);
-                postDetail.notifyDataSetChanged();
             }
 
             @Override
@@ -221,14 +216,7 @@ public class PostDetailActivity extends AppCompatActivity{
 
             }
         });
-
-    }*/
-
-    private void showMessage(String message) {
-
-        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
     }
-
 
     private String timestampToString(long time) {
 
